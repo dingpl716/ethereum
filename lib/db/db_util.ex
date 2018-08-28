91cee443cb87c7b24fc99c22a60545cfb890c79f
Peiling Ding <peding@arcblock.io>
2018-08-28 16:17:35 -0700

    Change db path

--- lib/db/db_util.ex
+++ lib/db/db_util.ex
@@ -1,7 +1,7 @@
 defmodule DbUtil do
   alias Rox
 
-  @db_path "/var/data/parity/chains/ethereum/db/906a34e69aec8c0d/archive"
+  @db_path "/var/data/parity/chains/ethereum/db/906a34e69aec8c0d/archive/db"
   def open_db do
     Rox.open(@db_path)
   end
