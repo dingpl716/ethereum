7201c5c6bc53e0a7917dc2fb9b681d5e4724bff5
Peiling Ding <peding@arcblock.io>
2018-08-31 15:51:38 -0700

    Add search trie

--- lib/merkle_patricia_trie/state_mpt.ex
+++ lib/merkle_patricia_trie/state_mpt.ex
@@ -3,11 +3,16 @@ defmodule StateMPT do
   alias StateDb
 
   def get_account(address, state_root) do
-    bin_block_hash = Util.format_key(state_root)
     addr_hash = :keccakf1600.hash(:sha3_256, Util.format_key(address))
+    search_trie(addr_hash, state_root)
+  end
+
+  def search_trie(key, root) do
+    bin_root = Util.format_key(root)
+    bin_key = Util.format_key(key)
 
     StateDb.init()
-    |> Trie.new(bin_block_hash)
+    |> Trie.new(bin_state_root)
     |> Trie.get(addr_hash)
     |> Account.from_rlp()
   end
