d6642592534ffdc4db7883e920df923e7874d0b5
Peiling Ding <peding@arcblock.io>
2018-08-31 13:26:19 -0700

    Update state_mpt.

--- lib/merkle_patricia_trie/state_mpt.ex
+++ lib/merkle_patricia_trie/state_mpt.ex
@@ -2,8 +2,8 @@ defmodule StateMPT do
   alias MerklePatriciaTree.Trie
   alias StateDb
 
-  def get_account(address, block_hash) do
-    bin_block_hash = Util.format_key(block_hash)
+  def get_account(address, state_root) do
+    bin_block_hash = Util.format_key(state_root)
     addr_hash = :keccakf1600.hash(:sha3_256, Util.format_key(address))
 
     StateDb.init()
